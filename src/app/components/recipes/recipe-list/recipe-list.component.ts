4793f18e0600bddf789fa260acf667ef22d86c29
prusotamydv78 <prusotamkumaryadav78@gmail.com>
2024-11-06 04:18:44 +0545

    day one

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { log } from 'console';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
+  recipes: Recipe[] = [new Recipe('testName', 'this is a test for recipe', 'https://www.allrecipes.com/thmb/KzSZ5rtc8-ahZyIFKEy1avZd1lk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/67700_RichPastaforthePoorKitchen_ddmfs_2x1_2283-fb3330bd0d3b41c7bd1543336ad4833f.jpg')];
+  
+  
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(Recipe)
+    console.log(this.recipes);
+    
   }
-
 }
