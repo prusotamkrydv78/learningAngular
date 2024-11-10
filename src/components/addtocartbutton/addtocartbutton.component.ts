8221372c89e405238485b6fa28b072c8523aed90
prusotamydv78 <prusotamkumaryadav78@gmail.com>
2024-11-11 05:25:12 +0545

    working on template-form

---
+++ src/components/addtocartbutton/addtocartbutton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addtocartbutton',
+  templateUrl: './addtocartbutton.component.html',
+  styleUrls: ['./addtocartbutton.component.css']
+})
+export class AddtocartbuttonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
